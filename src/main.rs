3c90b56f2b613cd30a63b1cf0c972477948fbb0d
DanSabau21 <dan.sabau.dev21@gmail.com>
2022-08-21 15:36:00 +0300

    fixed some issues

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,91 @@
-use std::io::{Read, Write, ErrorKind};
-use std::net::{SocketAddr, TcpListener};
+use std::io::{ErrorKind, Read, Write};
+use std::net::TcpStream;
 use std::sync::mpsc;
-use std::sync::mpsc::Sender;
 use std::thread;
 
-const LOCAL_HOST: &str = "127.0.0.1:6000";
-const MAX_MSG_LENGTH: u8 = 32;
-const THREAD_SLEEP_TIME: u64 = 100;
-
-fn sleep(millis: u64) {
-    thread::sleep(std::time::Duration::from_millis(millis))
+mod constants {
+    pub const LOCAL_HOST: &str = "127.0.0.1:6000";
+    pub const MAX_MSG_LENGTH: usize = 32;
+    pub const DEFAULT_THREAD_SLEEP_MILLIS: u64 = 100;
 }
 
-fn listen_for_client_msgs(ip_addr: SocketAddr, sender: Sender<String>) {
-    loop {
-        let mut buffer = vec![0, MAX_MSG_LENGTH];
-        match socket.read_exact(&mut buffer) {
-            Ok(_) => {
-                let message = buffer
-                    .into_iter()
-                    .take_while(|&x| x != 0)
-                    .collect::<Vec<_>>();
+mod  utils {
 
-                let message = String::from_utf8(message)
-                    .expect("Invalid UTF-8 formatted message.");
+    pub fn sleep_for(millis: u64) {
+        std::thread::sleep(std::time::Duration::from_millis(millis));
+    }
+}
 
-                println!("{}: {:?}", ip_addr, message);
+mod server {
+    use std::net::TcpListener;
 
-                sender.send(message)
-                    .expect("Sender failed to send the message.");
-            },
-            Err(ref error) if ErrorKind::WouldBlock == error.kind() => {
-                continue; // this kind of error isn't important
-            }
-            Err(_) => {
-                println!("closing connection with client at: {}", ip_addr);
-                break;
-            }
-        }
+    use super::constants;
 
-        sleep(THREAD_SLEEP_TIME)
+    pub fn new_tcp_listener() -> TcpListener {
+        let tcp_listener = TcpListener::bind(constants::LOCAL_HOST)
+            .expect("TCP listener failed to bind.");
+
+        tcp_listener.set_nonblocking(true)
+            .expect("Failed to set non-blocking flag for the TCP listener");
+
+        return tcp_listener;
     }
 }
 
 fn main() {
-    let tcp_listener = TcpListener::bind(LOCAL_HOST)
-        .expect("TCP Listener failed to bind.");
+    let tcp_listener = server::new_tcp_listener();
+    let (sender, receiver) = mpsc::channel::<String>();
+    let mut client_tcp_streams: Vec<TcpStream> = vec![];
 
-    tcp_listener.set_nonblocking(true)
-        .expect("Failed to set nonblocking mode for the TCP Listener.");
-
-    let (
-        sender,
-        receiver
-    ) =
-        mpsc::channel::<String>();
-
-    let mut client_sockets = vec![];
+    println!("waiting for clients to connect...");
 
     loop {
-        if let Ok((mut socket, ip_addr)) = tcp_listener.accept() {
-            println!("client connected: {}", ip_addr);
+        if let Ok((mut client_tcp_stream, addr)) = tcp_listener.accept() {
+            println!("client connected: {}", addr);
 
-            let socket_clone = socket.try_clone()
-                .expect("Failed to clone a socket.");
+            let client_tcp_stream_clone = client_tcp_stream.try_clone()
+                .expect("Failed to clone the client's TCP stream.");
 
             let sender_clone = sender.clone();
 
-            client_sockets.push(socket_clone);
+            client_tcp_streams.push(client_tcp_stream_clone);
 
-            thread::spawn(move || {
-                listen_for_client_msgs(ip_addr, sender_clone)
+            thread::spawn(move || loop {
+                let mut buff = vec![0; constants::MAX_MSG_LENGTH];
+
+                match client_tcp_stream.read_exact(&mut buff) {
+                    Ok(_) => {
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+
+                        println!("{}: {:?}", addr, msg);
+                        sender_clone.send(msg).expect("failed to send msg to rx");
+                    },
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("closed connection with: {}", addr);
+                        break;
+                    }
+                }
+
+                utils::sleep_for(constants::DEFAULT_THREAD_SLEEP_MILLIS);
             });
         }
 
         if let Ok(message) = receiver.try_recv() {
-            client_sockets = client_sockets.into_iter().filter_map(
-                |mut client_socket| {
+            client_tcp_streams = client_tcp_streams.into_iter().filter_map(
+                |mut client_tcp_stream| {
                     let mut buffer = message.clone().into_bytes();
-                    buffer.resize(MAX_MSG_LENGTH as usize, 0);
+                    buffer.resize(constants::MAX_MSG_LENGTH, 0);
 
-                    return client_socket.write_all(&buffer)
-                        .map(|_| client_socket).ok();
+                    client_tcp_stream
+                        .write_all(&buffer)
+                        .map(|_| client_tcp_stream)
+                        .ok()
                 }
-            ).collect::<Vec<_>>()
+            ).collect::<Vec<_>>();
         }
 
-        sleep(THREAD_SLEEP_TIME)
+        utils::sleep_for(constants::DEFAULT_THREAD_SLEEP_MILLIS);
     }
 }
